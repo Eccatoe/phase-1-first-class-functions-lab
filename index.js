395547be0a0e2e4df435d33ca8c557cc732225e5
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-11 18:05:27 -0500

    commit tree 511c8f0e0e380662bd7b1a189c20fab6bf452b0c

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+
+
+const returnFirstTwoDrivers= function(driverArray){
+    return driverArray.slice(0,2);
+}
+
+
+const returnLastTwoDrivers=function(driverArray){
+    return driverArray.slice(-2)
+}
+
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare *  multiplier
+    } 
+}
+
+const fareDoubler=createFareMultiplier(2);
+
+const fareTripler=createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers, func){
+    // console.log("DRIVERS", drivers);
+    // console.log("FUNC", func);
+    return func(drivers)
+}
+
+// const driverArray=['Antonia', 'Nuru', 'Amari', 'Mo']
+// selectDifferentDrivers(driverArray, returnFirstTwoDrivers)
+
+
